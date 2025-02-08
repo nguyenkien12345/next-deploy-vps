5947930e6fc73ce7d23d36f4cc6da0220b544c45
Kien Nguyen Trung <kiennt@pivot.vn>
2025-02-08 17:52:51 +0700

    Update content

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -4,7 +4,7 @@ export function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog Of Fullstack Developer Nguyen Trung Kien
+        Blog Of Fullstack Developer/Depops Nguyen Trung Kien
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
         Dự án Deploy Next JS VPS của FullStack NTK{" "}
